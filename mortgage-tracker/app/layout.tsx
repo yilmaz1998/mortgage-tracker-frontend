6f0e5f992aa3e5d3d1ed06eb0379ec78dff2dd0b
yilmaz1998 <snn.ylmz9804@gmail.com>
2026-02-12 13:42:12 -0500

    feat(frontend): set up Next.js app with shadcn dark mode

---
+++ mortgage-tracker/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Mortgage Tracker",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+    <body>
+      {children}
+    </body>
+  </html>
+  );
+}
